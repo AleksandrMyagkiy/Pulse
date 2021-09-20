a125474581244fec2ce9befd5a99c13244a4daaf
Aleksandr <dentranscom@gmail.com>
2021-09-20 08:41:02 +0300

    slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,35 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
+//$(document).ready(function(){
+//    $('.carousel__inner').slick({
+//        speed: 1200,
+//        adaptiveHeight: true,
+//        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+//        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+//        responsive: [
+//            {
+//                breakpoint: 768,
+//                settings: {
+//                    dots: true,
+//                    arrows: false
+//                }
+//            }
+//        ]
+//      });
+//  });
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
-    });
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false
+});
 
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        })
-    })
-})
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev'); 
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next'); 
+});
+  
